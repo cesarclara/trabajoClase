e8be8fb5732110a8c7e7acacd6c828bb89f6ea02
Diurno <Diurno@13-C7>
2013-05-03 12:18:55 +0200

    proyecto1

---
+++ src/paqueteCesar/PruebaGIT2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteCesar;
+
+/**
+ *
+ * @author Diurno
+ */
+public class PruebaGIT2 {
+    public void hola()
+    {
+        System.out.println("Hola a todos");
+    }
+}
