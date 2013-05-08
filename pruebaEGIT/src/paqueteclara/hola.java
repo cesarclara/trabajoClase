05380d75f727c45962f77ee336c6d59c10637f8b
Diurno <Diurno@15-C7>
2013-05-08 13:03:39 +0200

    Borrando la nueva clase directamente al head (paquete Cesar) para luego recuperarla desde remoto

---
+++ pruebaEGIT/src/paqueteclara/hola.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteclara;
+
+/**
+ *
+ * @author Diurno
+ */
+public class hola {
+    
+    public void holaMundo()
+    {
+        System.out.println("Hola mundo");
+    }
+    
+}
+
